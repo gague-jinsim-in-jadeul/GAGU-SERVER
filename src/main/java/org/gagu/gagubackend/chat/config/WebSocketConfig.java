cdb1aa353da8eaa81a9d82775c69cbd14e4c8b96
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2024-09-15 14:55:17 +0900

    :bug: Change websocket config
    - SocketJS 사용하지 않는 설정으로 변경

--- src/main/java/org/gagu/gagubackend/chat/config/WebSocketConfig.java
+++ src/main/java/org/gagu/gagubackend/chat/config/WebSocketConfig.java
@@ -22,11 +22,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
         registry.addEndpoint("/chat-2d")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
     }
 
     /**
