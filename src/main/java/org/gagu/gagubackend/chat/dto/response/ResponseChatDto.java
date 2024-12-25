27d54659f5e2e2e3914dc0831ea0c8e57f2b5223
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2024-12-25 20:30:33 +0900

    🎨 change chat dto field

--- src/main/java/org/gagu/gagubackend/chat/dto/response/ResponseChatDto.java
+++ src/main/java/org/gagu/gagubackend/chat/dto/response/ResponseChatDto.java
@@ -2,10 +2,7 @@ package org.gagu.gagubackend.chat.dto.response;
 
 import lombok.*;
 
-import java.time.LocalDateTime;
 @Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @Builder
