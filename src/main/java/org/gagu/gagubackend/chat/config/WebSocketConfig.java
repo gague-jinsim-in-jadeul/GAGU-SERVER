23a8bd603296c32a95e5dbf54aab3b4f5bf1c8d4
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2024-12-25 20:30:01 +0900

    Update WebSocketConfig.java

--- src/main/java/org/gagu/gagubackend/chat/config/WebSocketConfig.java
+++ src/main/java/org/gagu/gagubackend/chat/config/WebSocketConfig.java
@@ -7,7 +7,6 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker // 메세지 브로커가 지원하는 WebSocket 메세지 처리 활성화
